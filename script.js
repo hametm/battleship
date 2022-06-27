308c80d8c2a48a6d073642f9c2a28d7a4f7a31a0
Morgan Hamet <morganhamet@gmail.com>
2022-06-27 12:16:45 -0700

    Add basic gameboard function

--- script.js
+++ script.js
@@ -10,4 +10,26 @@ function Ship(position) {
     return { length, hitPositions, hit, isSunk };
 }
 
-export { Ship }
+const Gameboard = (() => {
+
+    return { 
+        displayBoard: () => {
+        const letterString = "ABCDEFGHIJ";
+        const letters = letterString.split("");
+        const numberString = "1,2,3,4,5,6,7,8,9,10";
+        const numbers = numberString.split(",");
+        const positions = [];
+        for (let i = 0; i < letters.length; i++) {
+            for (let j = 0; j < numbers.length; j++) {
+                let position = String(letters[i]) + String(numbers[j]);
+                positions.push(position);
+            }
+        }
+        
+    } 
+};
+})();
+
+Gameboard.displayBoard();
+
+// export { Ship, Gameboard }
